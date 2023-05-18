122a2f444cf63fb77cd557af7259032e95aa404c
ankitmgs <85329746+ankitmgs@users.noreply.github.com>
2023-05-18 18:33:18 +0530

    Added PWA files

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import "@fortawesome/fontawesome-free/css/all.min.css";
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,6 +13,7 @@ root.render(
     <App />
   </React.StrictMode>
 );
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
